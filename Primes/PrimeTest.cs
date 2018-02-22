80ee7b180037d43d2d7c51b13ac060128b9d737e
Brian Vach <Brianvach@users.noreply.github.com>
2018-02-21 23:29:17 -0500

    Initial commit

---
+++ Primes/PrimeTest.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Primes
+{
+    [TestFixture]
+    class PrimeTest
+    {
+        //Tests for 1 thru 3 are to make sure that the core portions do not break
+        [Test]
+        public void ValidateOne()
+        {
+            string expected = "1: 1, ";
+            string output = Program.CalculatePrimes(1);
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void ValidateTwo()
+        {
+            string expected = "2: 2, ";
+            string output = Program.CalculatePrimes(2);
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void ValidateThree()
+        {
+            string expected = "3: 3, ";
+            string output = Program.CalculatePrimes(3);
+            Assert.AreEqual(expected, output);
+        }
+
+        //test the first prime that is not hard coded
+        [Test]
+        public void ValidateFive()
+        {
+            string expected = "5: 5, ";
+            string output = Program.CalculatePrimes(5);
+            Assert.AreEqual(expected, output);
+        }
+
+        //testing a very large prim
+        [Test]
+        public void ValidateLargePrime()
+        {
+            string expected = "15485863: 15485863, ";
+            string output = Program.CalculatePrimes(15485863);
+            Assert.AreEqual(expected, output);
+        }
+
+        //testing small non-prime
+        [Test]
+        public void ValidateSmallComposite()
+        {
+            string expected = "4: 2, 2, ";
+            string output = Program.CalculatePrimes(4);
+            Assert.AreEqual(expected, output);
+        }
+
+        //Validate large 2 based number
+        [Test]
+        public void ValidateLargeTwoBase()
+        {
+            string expected = "1073741824: ";
+            for (int i = 0; i < 30; i++)
+            {
+                expected += "2, ";
+            }
+            string output = Program.CalculatePrimes(1073741824);
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void ValidateIntMax()
+        {
+            //This number is prime, and should only return itself
+            //This number should test how quickly the algorithm is for iterating through possible primes
+            string expected = int.MaxValue.ToString();
+            expected += ": 2147483647, ";
+            string output = Program.CalculatePrimes(2147483647);
+            Assert.AreEqual(expected, output);
+        }
+
+        //ensure that the method is able to iterate and add multiple prime numbers.
+        [Test]
+        public void ValidatUniquePrimeNumber()
+        {
+           
+            string expected = "223092870: 2, 3, 5, 7, 11, 13, 17, 19, 23, ";
+            string output = Program.CalculatePrimes(223092870);
+            Assert.AreEqual(expected, output);
+        }
+
+        //Make sure that it is able to handle a number that is perfectly squared.
+        [Test]
+        public void ValidateDoubleHigherPrime()
+        {
+            string expected = "49: 7, 7, ";
+            string output = Program.CalculatePrimes(49);
+            Assert.AreEqual(expected, output);
+        }
+        
+        //same as above, but with a number that is to the 4th power.
+        [Test]
+        public void TestNumberToFourth()
+        {
+            string expected = "625: 5, 5, 5, 5, ";
+            string output = Program.CalculatePrimes(625);
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void TestNegativeNumber()
+        {
+            //while 2 * 2 != -4, -4 contains two 2s. This requires that -1 is displayed, or that it gives the incorrect result
+            //I will continue to use the same logic as was provided.
+            string expected = "-4: 2, 2, ";
+            string output = Program.CalculatePrimes(-4);
+            Assert.AreEqual(expected, output);
+        }
+
+
+        //no need to test passing a non-integer value, as the program will not compile
+    }
+}
---
+++ Primes/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace Primes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ls;
+            //put the initial command into a try catch to ensure the program is able to close gracefully
+            try
+            {
+                ls = File.ReadAllLines(args[0]);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Please pass the path as an argument");
+                return; //Use return to exit the program, no need to continue running the application as it is run from the command line.
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Path does not lead to a valid file");
+                return;
+            }
+
+            for (int i = 0; i < ls.Length; i++)
+            {
+                string l = ls[i];
+                int n = int.Parse(l);
+                string output = CalculatePrimes(n); //Refactor into seperate method, easier for testing
+                Console.WriteLine(output);
+            }
+        }
+
+        //Refactor actual calculation to a seperate method.
+        public static string CalculatePrimes(int n)
+        {
+            string output = n + ": "; //create output string to return to the application
+            if (n == 1)
+            {
+                return output + "1, "; // Is 1 a prime number??? Debated, but for the sake of this program, it will be assumed so.
+                //return the value since 1 is the most that it will be.
+            }
+            else
+            {
+                //keep the 2 specific method, as it is the only even prime number
+                while (n % 2 == 0)
+                {
+                    n /= 2;
+                    output += "2, ";
+                }
+
+                //removed the specific method for 3, as it is redundant in the next part of the function
+
+                int x = (int)Math.Floor(Math.Sqrt(n)) + 1;
+                for (int j = 3; j < x && n != 1; j += 2) //Added test to see if number has already searched for all primes
+                {
+                    while (n % j == 0)
+                    {
+                        n /= j;
+                        output += j.ToString();
+                        output += ", ";
+                    }
+                }
+
+                if (n > 1)
+                {
+                    output += n.ToString();
+                    output += ", ";
+                }
+            }
+            return output;
+        }
+    }
+}

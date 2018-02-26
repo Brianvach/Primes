6385aa0ee2a2926a22a31777e9b008c73c574b8b
Brian Vach <Brianvach@users.noreply.github.com>
2018-02-26 18:47:24 -0500

    Old main function removed
    Main function only handles getting the input and instructing the program to prinit out the results.

--- Primes/Program.cs
+++ Primes/Program.cs
@@ -6,34 +6,6 @@ namespace Primes
 {
     class Program
     {
-        static void OldMain(string[] args)
-        {
-            string[] ls;
-            //put the initial command into a try catch to ensure the program is able to close gracefully
-            try
-            {
-                ls = File.ReadAllLines(args[0]);
-            }
-            catch (IndexOutOfRangeException)
-            {
-                Console.WriteLine("Please pass the path as an argument");
-                return; //Use return to exit the program, no need to continue running the application as it is run from the command line.
-            }
-            catch (FileNotFoundException)
-            {
-                Console.WriteLine("Path does not lead to a valid file");
-                return;
-            }
-
-            for (int i = 0; i < ls.Length; i++)
-            {
-                string l = ls[i];
-                int n = int.Parse(l);
-                string output = CalculatePrimes(n); //Refactor into seperate method, easier for testing
-                Console.WriteLine(output);
-            }
-        }
-
         static void Main(string[] args)
         {
             Prime prime;
